6ffc36bae9d182d9a26c86bb8b249c34053d691e
DuskyElf <91879372+DuskyElf@users.noreply.github.com>
2025-10-30 15:36:03 +0530

    feat!: telescope remastered

--- nvim/plugin/telescope.lua
+++ nvim/plugin/telescope.lua
@@ -5,8 +5,24 @@ vim.g.did_load_telescope_plugin = true
 
 local telescope = require('telescope')
 local actions = require('telescope.actions')
-
 local builtin = require('telescope.builtin')
+
+telescope.setup {
+  pickers = {
+    find_files = {
+      theme = "ivy"
+    },
+    git_files = {
+      theme = "ivy"
+    },
+    buffers = {
+      theme = "ivy"
+    },
+    spell_suggest = {
+      theme = "ivy"
+    }
+  }
+}
 
 local layout_config = {
   vertical = {
@@ -30,79 +46,23 @@ local project_files = function()
   end
 end
 
----@param picker function the telescope picker to use
-local function grep_current_file_type(picker)
-  local current_file_ext = vim.fn.expand('%:e')
-  local additional_vimgrep_arguments = {}
-  if current_file_ext ~= '' then
-    additional_vimgrep_arguments = {
-      '--type',
-      current_file_ext,
-    }
-  end
-  local conf = require('telescope.config').values
-  picker {
-    vimgrep_arguments = vim.tbl_flatten {
-      conf.vimgrep_arguments,
-      additional_vimgrep_arguments,
-    },
-  }
-end
-
---- Grep the string under the cursor, filtering for the current file type
-local function grep_string_current_file_type()
-  grep_current_file_type(builtin.grep_string)
-end
-
---- Live grep, filtering for the current file type
-local function live_grep_current_file_type()
-  grep_current_file_type(builtin.live_grep)
-end
-
 --- Like live_grep, but fuzzy (and slower)
 local function fuzzy_grep(opts)
   opts = vim.tbl_extend('error', opts or {}, { search = '', prompt_title = 'Fuzzy grep' })
   builtin.grep_string(opts)
 end
 
-local function fuzzy_grep_current_file_type()
-  grep_current_file_type(fuzzy_grep)
-end
-
-vim.keymap.set('n', '<leader>tp', function()
-  builtin.find_files()
-end, { desc = '[t]elescope find files - ctrl[p] style' })
-vim.keymap.set('n', '<M-p>', builtin.oldfiles, { desc = '[telescope] old files' })
+vim.keymap.set('n', '<C-p>', builtin.oldfiles, { desc = '[telescope] old files' })
 vim.keymap.set('n', '<C-g>', builtin.live_grep, { desc = '[telescope] live grep' })
-vim.keymap.set('n', '<leader>tf', fuzzy_grep, { desc = '[t]elescope [f]uzzy grep' })
-vim.keymap.set('n', '<M-f>', fuzzy_grep_current_file_type, { desc = '[telescope] fuzzy grep filetype' })
-vim.keymap.set('n', '<M-g>', live_grep_current_file_type, { desc = '[telescope] live grep filetype' })
-vim.keymap.set(
-  'n',
-  '<leader>t*',
-  grep_string_current_file_type,
-  { desc = '[t]elescope grep current string [*] in current filetype' }
-)
+vim.keymap.set('n', '<leader>:', builtin.builtin, { desc = 'telescope [:] builtins' })
+vim.keymap.set('n', '<leader>o', project_files, { desc = 'telescope [o]pen file' })
+vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'telescope [b]uffers' })
+vim.keymap.set('n', '<leader>fg', fuzzy_grep, { desc = 'telescope [f]uzzy [g]rep' })
+vim.keymap.set('n', '<leader>fs', builtin.spell_suggest, { desc = 'telescope [f]ix [s]pell' })
 vim.keymap.set('n', '<leader>*', builtin.grep_string, { desc = '[telescope] grep current string [*]' })
-vim.keymap.set('n', '<leader>tg', project_files, { desc = '[t]elescope project files [g]' })
-vim.keymap.set('n', '<leader>tc', builtin.quickfix, { desc = '[t]elescope quickfix list [c]' })
-vim.keymap.set('n', '<leader>tq', builtin.command_history, { desc = '[t]elescope command history [q]' })
-vim.keymap.set('n', '<leader>tl', builtin.loclist, { desc = '[t]elescope [l]oclist' })
-vim.keymap.set('n', '<leader>tr', builtin.registers, { desc = '[t]elescope [r]egisters' })
-vim.keymap.set('n', '<leader>tbb', builtin.buffers, { desc = '[t]elescope [b]uffers [b]' })
-vim.keymap.set(
-  'n',
-  '<leader>tbf',
-  builtin.current_buffer_fuzzy_find,
-  { desc = '[t]elescope current [b]uffer [f]uzzy find' }
-)
-vim.keymap.set('n', '<leader>td', builtin.lsp_document_symbols, { desc = '[t]elescope lsp [d]ocument symbols' })
-vim.keymap.set(
-  'n',
-  '<leader>to',
-  builtin.lsp_dynamic_workspace_symbols,
-  { desc = '[t]elescope lsp dynamic w[o]rkspace symbols' }
-)
+vim.keymap.set('n', '<leader>qf', builtin.quickfix, { desc = 'telescope [q]uick[f]ix list' })
+vim.keymap.set('n', '<leader>ch', builtin.command_history, { desc = 'telescope [c]ommand [h]istory' })
+vim.keymap.set('n', '<leader>s', builtin.lsp_document_symbols, { desc = 'telescope lsp document [s]ymbols' })
 
 telescope.setup {
   defaults = {
@@ -115,7 +75,7 @@ telescope.setup {
       i = {
         ['<C-q>'] = actions.send_to_qflist,
         ['<C-l>'] = actions.send_to_loclist,
-        -- ['<esc>'] = actions.close,
+        ['<esc>'] = actions.close,
         ['<C-s>'] = actions.cycle_previewers_next,
         ['<C-a>'] = actions.cycle_previewers_prev,
       },
