6c0499e8341abdaef919cbb3eff5341d0b861eb4
DuskyElf <91879372+DuskyElf@users.noreply.github.com>
2025-12-12 14:28:26 +0530

    feat: Let's GO

--- nvim/plugin/rustdev.lua
+++ nvim/plugin/rustdev.lua
@@ -2,3 +2,9 @@ vim.lsp.config('rust_analyzer', {
     capabilities = require('user.lsp').make_client_capabilities(),
 })
 vim.lsp.enable("rust_analyzer")
+
+
+vim.lsp.config('gopls', {
+    capabilities = require('user.lsp').make_client_capabilities(),
+})
+vim.lsp.enable('gopls')
